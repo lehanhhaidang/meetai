75c29418b486c71c9901e98f48e005bcecda36af
Lê Hạnh Hải Đăng <110990682+lehanhhaidang@users.noreply.github.com>
2025-10-14 04:15:39 +0700

    05-authentication-social

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,20 @@
-"use client"
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { authClient } from "@/lib/auth-client";
-import Image from "next/image";
-import { useState } from "react";
+import { auth } from "@/lib/auth";
+import HomeView from "@/modules/home/ui/views/home-view";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 
-export default function Home() {
-  const { 
-        data: session, 
-    } = authClient.useSession() 
+const Page = async () => {
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-  const onSubmit = () => {
-    authClient.signUp.email({
-      email,
-      name,
-      password
-    },{
-      onError:()=>{
-        window.alert("Something went wrong")
-      },
-      onSuccess:()=>{
-        window.alert("User created successfully")
-      }
-    });
+  if(!session) {
+    redirect("/sign-in");
   }
-  const onLogin = () => {
-    authClient.signIn.email({
-      email,
-      password
-    },{
-      onError:()=>{
-        window.alert("Something went wrong")
-      },
-      onSuccess:()=>{
-        window.alert("Logged in successfully")
-      }
-    });
-  }
-  if(session){
-    return (
-      <div className="p-4 flex flex-col gap-y-4">
-        <p>Logged in as {session.user.name}</p>
-        <Button onClick={()=> authClient.signOut()}>
-          Sign Out
-        </Button>
-      </div>
-    );
-  }
-  return (
-    <div className="flex flex-col gap-y-4">
-      <div className="p-4 flex flex-col gap-y-4">
-        <Input 
-          placeholder="name" 
-          value={name} 
-          onChange={(e)=> setName(e.target.value)}>
-        </Input>
-        <Input 
-          placeholder="email" 
-          value={email} 
-          onChange={(e)=> setEmail(e.target.value)}>
-        </Input>
-        <Input
-          type="password"
-          placeholder="password" 
-          value={password} 
-          onChange={(e)=> setPassword(e.target.value)}>
-        </Input>
-        <Button onClick={onSubmit}>
-          Create User
-        </Button>
-      </div>
-      <div className="p-4 flex flex-col gap-y-4">
-        <Input 
-          placeholder="email" 
-          value={email} 
-          onChange={(e)=> setEmail(e.target.value)}>
-        </Input>
-        <Input
-          type="password"
-          placeholder="password" 
-          value={password} 
-          onChange={(e)=> setPassword(e.target.value)}>
-        </Input>
-        <Button onClick={onLogin}>
-          Login
-        </Button>
-      </div>
-    </div>
-
-  );
+  return ( 
+    <HomeView />
+   );
 }
+ 
+export default Page;
--- src/modules/auth/ui/view/sign-in-view.tsx
+++ src/modules/auth/ui/view/sign-in-view.tsx
@@ -16,8 +16,9 @@ import { Alert, AlertTitle } from "@/components/ui/alert";
 import { useForm } from "react-hook-form";
 import { authClient } from "@/lib/auth-client";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import {FaGithub, FaGoogle} from "react-icons/fa";
 
 const formSchema = z.object({
     email: z.email(),
@@ -47,11 +48,12 @@ export const SignInView = () => {
             {
                 email: data.email,
                 password: data.password,
+                callbackURL: "/"
             },
             {
                 onSuccess: () => {
                     setPending(false);
-                    router.push("/");
+                    router.push("/")
                 },
                 onError: ({error}) => {
                     setPending(false);
@@ -59,7 +61,27 @@ export const SignInView = () => {
                 }
             },
        )
-    }
+    };
+    const onSocial = async (provider: "google" | "github") => {
+        setError(null);
+        setPending(true);
+
+        authClient.signIn.social(
+            {
+                provider: provider,
+                callbackURL: "/"
+            },
+            {
+                onSuccess: () => {
+                    setPending(false);
+                },
+                onError: ({error}) => {
+                    setPending(false);
+                    setError(error.message);
+                }
+            },
+       )
+    };
     return (
         <div className="flex flex-col gap-6">
             <Card className="overflow-hidden p-0">
@@ -134,24 +156,26 @@ export const SignInView = () => {
                                 <div className="grid grid-cols-2 gap-4">
                                     <Button
                                         disabled={pending}
+                                        onClick={()=>{onSocial("google")}}
                                         variant="outline"
                                         className="w-full"
                                         type="button">
-                                            Google
+                                        <FaGoogle/>
                                     </Button>
                                      <Button
                                         disabled={pending}
+                                        onClick={()=>{onSocial("github")}}
                                         variant="outline"
                                         className="w-full"
                                         type="button">
-                                            Github
+                                        <FaGithub/>
                                     </Button>
                                 </div>
                                 <div className="text-center text-sm">
                                     Don&apos;t have an account?{" "}
                                     <Link
                                         href="/sign-up"
-                                        className="underline underline-offset-4"
+                                        className="underline underline-offset-4 hover:font-bold"
                                     >
                                         Sign Up
                                     </Link>
