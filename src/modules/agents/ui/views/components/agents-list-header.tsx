0af7cd73782b43d496f968801a4797e607875a0b
Lê Hạnh Hải Đăng <110990682+lehanhhaidang@users.noreply.github.com>
2025-11-06 23:44:50 +0700

    19-meetings-filters

--- src/modules/agents/ui/views/components/agents-list-header.tsx
+++ src/modules/agents/ui/views/components/agents-list-header.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { useAgentsFilters } from "@/modules/agents/hooks/use-agents-filters";
 import { AgentsSearchFilters } from "./agents-search-filters";
 import { DEFAULT_PAGE } from "@/constants";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export const AgentsListHeader = () => {
 
@@ -31,14 +32,20 @@ export const AgentsListHeader = () => {
                         New Agent
                     </Button>
                 </div>
-                <div className="flex items-center gap-y-2 p-1">
-                    <AgentsSearchFilters />
-                    {isAnyFilterModified && (
-                        <Button variant="outline" size="sm" onClick={onClearFilters}>
-                            <XCircleIcon />
-                        </Button>
-                    )}
-                </div>
+                <ScrollArea>
+                    <div className="flex items-center gap-y-2 p-1">
+                        <AgentsSearchFilters />
+                        {isAnyFilterModified && (
+                            <Button variant="outline" size="sm" onClick={onClearFilters}>
+                                <XCircleIcon />
+                                Clear
+                            </Button>
+                        )}
+                    </div>
+                    <ScrollBar
+                        orientation="horizontal"
+                    />
+                </ScrollArea>
             </div>
         </>
     );
--- src/modules/meetings/ui/views/components/meetings-list-header.tsx
+++ src/modules/meetings/ui/views/components/meetings-list-header.tsx
@@ -1,13 +1,32 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { PlusIcon } from "lucide-react";
+import { PlusIcon, XCircleIcon } from "lucide-react";
 import { useState } from "react";
 import { NewMeetingDialog } from "./new-meeting-dialog";
+import { MeetingsSearchFilters } from "./meetings-search-filters";
+import { StatusFilter } from "./status-filters";
+import { AgentIdFilter } from "./agent-id-filter";
+import { useMeetingsFilters } from "@/modules/meetings/hooks/use-meetings-filters";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Scrollbar } from "@radix-ui/react-scroll-area";
+import { DEFAULT_PAGE } from "@/constants";
 
 
 export const MeetingsListHeader = () => {
 
+    const [filters, setFilters] = useMeetingsFilters();
     const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+    const isAnyFilterModified = !!filters.status || !!filters.agentId || !!filters.search;
+
+    const onClearFilters = () => {
+        setFilters({
+            status: null,
+            agentId: "",
+            search: "",
+            page: DEFAULT_PAGE,
+        })
+    }
 
     return (
 
@@ -21,9 +40,22 @@ export const MeetingsListHeader = () => {
                         New Meeting
                     </Button>
                 </div>
-                <div className="flex items-center gap-y-2 p-1">
-                    TODO: filters
-                </div>
+                <ScrollArea>
+                    <div className="flex items-center gap-x-2 p-1">
+                        <MeetingsSearchFilters />
+                        <StatusFilter />
+                        <AgentIdFilter />
+                        {isAnyFilterModified && (
+                            <Button variant="outline" onClick={onClearFilters}>
+                                <XCircleIcon className="size-4" />
+                                Clear
+                            </Button>
+                        )}
+                    </div>
+                    <Scrollbar
+                        orientation="horizontal"
+                    />
+                </ScrollArea>
             </div>
         </>
     );
